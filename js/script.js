8950c2b24a550b7a58bcdd9a316853032912f93d
trieu <trieu.tq20194692@sis.hust.edu.vn>
2023-02-11 22:31:30 +0700

    up load cua trieu

--- js/script.js
+++ js/script.js
@@ -8,3 +8,34 @@ itemsliderbar.forEach(function(menu,index)
     })
 })
 // -----------------product--------------
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smallImg = document.querySelectorAll(".product-content-left-small-img img")
+smallImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click",function(){
+        bigImg.src = imgItem.src
+    })
+})
+
+const noidung = document.querySelector(".noidung")
+const tacgia = document.querySelector(".tacgia")
+if(noidung){
+    noidung.addEventListener("click",function()
+    {
+        document.querySelector(".product-content-right-bottom-content-tacgia").style.display="none"
+        document.querySelector(".product-content-right-bottom-content-noidung").style.display="block"
+    })
+}
+if(tacgia){
+    tacgia.addEventListener("click",function()
+    {
+        document.querySelector(".product-content-right-bottom-content-tacgia").style.display="block"
+        document.querySelector(".product-content-right-bottom-content-noidung").style.display="none"
+    })
+}
+const butTon = document.querySelector(".product.content-right-bottom-top")
+if(botTom){
+    botTom.addEventListener("click",function()
+    {
+        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB")
+    })
+}
